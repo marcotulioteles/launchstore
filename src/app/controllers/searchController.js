4549cf74643d9da629bb607e3eaa90d54ab108b4
marcotulioteles <marcotuliocivileng@gmail.com>
2021-01-07 23:21:03 -0300

    commit after merge files

--- src/app/controllers/searchController.js
+++ src/app/controllers/searchController.js
@@ -1,18 +1,10 @@
 const { formatPrice, date } = require('../../lib/utils');
 const Product = require('../models/product')
-<<<<<<< HEAD
-=======
-const File = require('../models/file')
->>>>>>> a55b90d16e27559400a5f0499b86a0447eb1fc55
 
 module.exports = {
   async index(req, res) {
     try {
-<<<<<<< HEAD
       let results
-=======
-      let results,
->>>>>>> a55b90d16e27559400a5f0499b86a0447eb1fc55
         params = {}
 
       const { filter, category } = req.query
@@ -30,38 +22,22 @@ module.exports = {
       async function getImage(productId) {
         let results = await Product.files(productId)
         const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public","")}`)
-<<<<<<< HEAD
   
         return files[0]
       }      
 
       const productPromise = results.rows.map(async product => {
-=======
-        
-        return files[0]
-      }
-
-      const productsPromise = results.rows.map(async product => {
->>>>>>> a55b90d16e27559400a5f0499b86a0447eb1fc55
         product.img = await getImage(product.id)
         product.oldPrice = formatPrice(product.old_price)
         product.price = formatPrice(product.price)
         return product
       })
 
-<<<<<<< HEAD
       const products = await Promise.all(productPromise)
      
       const search = {
         term: req.query.filter,
         total: products.length
-=======
-      const products = await Promise.all(productsPromise)
-
-      const search = {
-        term: req.query.filter,
-        total: products.length 
->>>>>>> a55b90d16e27559400a5f0499b86a0447eb1fc55
       }
 
       const categories = products.map(product => ({
@@ -77,18 +53,11 @@ module.exports = {
       }, [])
 
       return res.render("search/index", { products, search, categories })
-<<<<<<< HEAD
   
     }
     catch (err) {
       console.error(err)
     }
 
-=======
-    }
-    catch(err) {
-
-    }
->>>>>>> a55b90d16e27559400a5f0499b86a0447eb1fc55
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -26,6 +26,4 @@ routes.get('/ads/create', function(req, res){
   return res.redirect('/products/create')
 })
 
-
-
 module.exports = routes
