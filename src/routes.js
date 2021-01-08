b56b39e3a0d2d9315f8fa8939445f31b693fadcf
marcotulioteles <marcotuliocivileng@gmail.com>
2021-01-07 22:51:58 -0300

    complete creation of search products

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,15 @@ const multer = require('./app/middlewares/multer')
 
 const productController = require('./app/controllers/productController')
 const homeController = require('./app/controllers/homeController')
+const searchController = require('./app/controllers/searchController')
 
+//Home
 routes.get('/', homeController.index)
 
+//Search
+routes.get('/products/search', searchController.index)
+
+//Products
 routes.get('/products/create', productController.create)
 routes.get('/products/:id', productController.show)
 routes.get('/products/:id/edit', productController.edit)
